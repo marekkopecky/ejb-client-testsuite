b011193a24ad430b76150654e0ae40506f334838
Marek Kopecky <mkopecky@redhat.com>
2024-08-09 13:19:49 +0200

    Improvements

--- .github/workflows/integration.sh
+++
@@ -1,4 +0,0 @@
-echo "INTEGRATION TESTS"
-
-mvn --batch-mode --no-transfer-progress package -DspecificModule=prepare
-mvn --batch-mode --no-transfer-progress dependency:tree clean verify --fail-at-end -Dmaven.test.redirectTestOutputToFile=true
